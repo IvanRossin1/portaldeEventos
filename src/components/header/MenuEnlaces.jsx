ba071f85784d2c9404c450a6246ad0714206097d
IvanRossin1 <ivannrossini@gmail.com>
2023-08-02 20:58:15 -0300

    asdasd

---
+++ src/components/header/MenuEnlaces.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MenuEnlaces = () => {
+  return (
+    <>
+        <ul>
+            <li><a href="http:#">Categorías</a></li>
+            <li><a href="http:#">Salones</a></li>
+            <li><a href="http:#">Imagen Personal</a></li>
+            <li><a href="http:#">Contacto</a></li>
+        </ul>
+    </>
+  )
+}
+
+export default MenuEnlaces
---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuEnlaces from './MenuEnlaces'
+
+const Encabezado = () => {
+  return (
+    <>
+      <div>PORTAL DE EVENTOS</div>
+      <MenuEnlaces />
+      <Usuarios />
+    </>
+  )
+}
+
+export default Encabezado
